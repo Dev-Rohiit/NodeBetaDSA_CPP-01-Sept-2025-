36bb59e78b168b5568a5b52630bf1c9c1ec520ab
Dev-Rohiit <kushwaharohit908@gmail.com>
2025-09-25 10:38:10 +0530

    patterns and Operator

---
+++ Lecture 15/002BitWiseOperator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int a = 25;
+    // int b = 19;
+    // cout<<"a & b : " << (a&b)<<endl;
+    // cout<<"a | b : " << (a|b)<<endl;
+    // cout<<"a ^ b : " << (a^b)<<endl;
+
+    int a = 1;
+    cout<<"a << b : " << (a = a<<3)<<endl;
+
+    a = 8;
+    cout<<"a >> b : " << (a = a>>3)<<endl;
+
+
+
+
+
+}
